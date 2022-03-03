9e1e6c2ba0faa90e1d2f4515052e1e2397b48b36
たのけん <tanoken729@gmail.com>
2022-03-03 23:18:28 +0900

    update uesr(crud)

--- routes/users.js
+++ routes/users.js
@@ -36,4 +36,46 @@ router.post("/add", (req, res, next) => {
     });
 });
 
+router.get("/edit", (req, res, next) => {
+  db.User.findByPk(req.query.id).then((usr) => {
+    var data = {
+      title: "Users/Edit",
+      form: usr,
+    };
+    res.render("users/edit", data);
+  });
+});
+
+// whereでクエリを使って
+// router.post("/edit", (req, res, next) => {
+//   db.sequelize
+//     .sync()
+//     .then(() =>
+//       db.User.update(
+//         {
+//           name: req.body.name,
+//           pass: req.body.pass,
+//           mail: req.body.mail,
+//           age: req.body.age,
+//         },
+//         {
+//           where: { id: req.body.id },
+//         }
+//       )
+//     )
+//     .then((usr) => {
+//       res.redirect("/users");
+//     });
+// });
+// 送られてきたidの値を使ってモデルを取り出す方法
+router.post("/edit", (req, res, next) => {
+  db.User.findByPk(req.body.id).then((usr) => {
+    usr.name = req.body.name;
+    usr.pass = req.body.pass;
+    usr.mail = req.body.mail;
+    usr.age = req.body.age;
+    usr.save().then(() => res.redirect("/users"));
+  });
+});
+
 module.exports = router;
